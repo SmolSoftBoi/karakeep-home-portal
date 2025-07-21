849c57634614c41bd3c357b8923d1c07c6bf9cd9
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2025-07-21 21:17:33 +0100

    test(ci): enable coverage

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      lines: 90,
+      branches: 90,
+      statements: 90,
+      functions: 90,
+      exclude: [
+        '**/layout.tsx',
+        '**/page.tsx',
+        'next.config.ts',
+        'postcss.config.mjs',
+        '.pnp.cjs',
+        '.pnp.loader.mjs',
+      ],
+    },
+  },
+});
