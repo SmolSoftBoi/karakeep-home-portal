154a8d3e88a811a05a0b6ca04279be5a95f99119
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2025-07-22 08:50:47 +0100

    fix(build): use tailwind postcss plugin

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
+import tailwind from '@tailwindcss/postcss';
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+  plugins: [tailwind],
 };
 
 export default config;
